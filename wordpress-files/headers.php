65b82e56d6214f210e361a531bad4748fe297c89
Ubuntu <semestral@semestral1b.atm4rfy2p2kejd2m25li1dr3bc.jx.internal.cloudapp.net>
2020-07-15 09:10:16 +0000

    v3

---
+++ wordpress-files/headers.php
@@ -0,0 +1,14 @@
+<?php
+// file headers.php
+header('Cache-Control: no-cache,must-revalidate');
+header('X-Clacks-Overhead: "GNU Terry Pratchett"');
+header('X-XSS-Protection: 1;mode=block');
+header('X-Content-Type-Options: nosniff');
+header('X-Frame-Options: SAMEORIGIN');
+header('Expect-CT: enforce,max-age=2592000');
+header('Content-Language: en');
+header('Referrer-Policy: origin-when-cross-origin');
+Header('Feature-Policy: geolocation=self; vibrate=none');
+Header('Content-Security-Policy: script-src=self'); 
+?>
+
